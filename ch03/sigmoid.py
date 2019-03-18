80c8c17ef83c9154e314ede7b062b73f0681dace
masaaki008 <uver.railgun.next@gmail.com>
2019-03-19 00:47:25 +0900

    new

---
+++ ch03/sigmoid.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+if __name__ == "__main__":
+    X = np.arange(-5.0, 5.0, 0.1)
+    Y = sigmoid(X)
+    print(Y)
+    plt.plot(X, Y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
